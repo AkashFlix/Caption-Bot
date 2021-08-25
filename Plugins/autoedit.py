3097164ab87a9183db743a656adb6e5d2d4cd376
Mr. Stark <75563336+tonyxstark@users.noreply.github.com>
2021-08-25 10:57:01 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -25,11 +25,11 @@ async def editing(bot, message):
          pass 
       if (message.document or message.video or message.audio): 
           if message.caption:                        
-             file_caption = f"**{message.caption}**"                
+             file_caption = f"<b>{message.caption}</b>"                
           else:
              fname = media.file_name
              filename = fname.replace("_", ".")
-             file_caption = f"`{filename}`"  
+             file_caption = f"<code>{filename}</code>"  
               
       try:
           if caption_position == "top":
@@ -37,21 +37,21 @@ async def editing(bot, message):
                  chat_id = message.chat.id, 
                  message_id = message.message_id,
                  caption = caption_text + file_caption,
-                 parse_mode = "markdown"
+                 parse_mode = "html"
              )
           elif caption_position == "bottom":
              await bot.edit_message_caption(
                  chat_id = message.chat.id, 
                  message_id = message.message_id,
                  caption = file_caption + "\n" + caption_text,
-                 parse_mode = "markdown"
+                 parse_mode = "html"
              )
           elif caption_position == "nil":
              await bot.edit_message_caption(
                  chat_id = message.chat.id,
                  message_id = message.message_id,
                  caption = caption_text, 
-                 parse_mode = "markdown"
+                 parse_mode = "html"
              ) 
       except:
           pass
